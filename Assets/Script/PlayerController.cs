0d45f7f1e013128e07c58969946c1b46e57ca642
sinewave0201 <silver520060201@gmail.com>
2026-01-25 18:05:50 -0500

    jump

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,10 +4,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {   [SerializeField]
     public float speed = 5f;
+    public float jumpForce = 5f;
+    private float jumpCooldown = 0.5f;
+    private float lastJumpTime;
+    public LayerMask groundLayer;
 
     [SerializeField]
     private Rigidbody rb;
     private Vector2 input;
+
+    [SerializeField]
+    private bool isGrounded = true;
 
     void Start()
     {
@@ -16,12 +23,38 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        
+        groundCheck();
+    }
+
+    void groundCheck()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, 
+            transform.TransformDirection(Vector3.down), out hit, 
+            0.25f, groundLayer))
+            {
+                Debug.Log("isGrounded = true");
+                Debug.DrawRay(transform.position, 
+                            transform.TransformDirection(Vector3.down) * hit.distance, 
+                            Color.yellow);
+                isGrounded = true;
+            }
+        else 
+        {
+            Debug.Log("isGrounded = false");
+            isGrounded = false;
+        }
     }
 
     public void jump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump!");
+        Debug.Log("jump is pressed");
+        if (isGrounded && Time.time > lastJumpTime + jumpCooldown)
+        {
+            lastJumpTime = Time.time;
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
